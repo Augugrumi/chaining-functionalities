6f7c6fae3fe20bc16aca09c1f439cb21c9ca4187
mzanella <zanna0150@gmail.com>
2018-07-31 11:08:52 +0200

    Add comments to Server

--- src/main/java/httputils/Server.java
+++ src/main/java/httputils/Server.java
@@ -3,7 +3,23 @@ package httputils;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+/**
+ * Basic interface that exposes methods for a server object
+ */
 public interface Server {
+
+    /**
+     * Method to wait for a connection and retrieve data as an HttpPacket object
+     * @param ss ServerSocket to which requests are retrieved
+     * @return HttpPacket that represent the request
+     * @throws IOException if connection breaks
+     */
     HttpPacket receive(ServerSocket ss) throws IOException;
+
+    /**
+     * Method to send a POST request
+     * @param message String that represent the message that will be sent
+     * @param destination String that represent the address of the receiver
+     */
     void send(String message, String destination);
 }
