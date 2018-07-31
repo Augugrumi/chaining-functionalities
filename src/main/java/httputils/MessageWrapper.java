c3b4ba54cd3ed3bb2ee3727654c093522f112b6f
mzanella <zanna0150@gmail.com>
2018-07-31 11:06:35 +0200

    Add comments to MessageWrapper

--- src/main/java/httputils/MessageWrapper.java
+++ src/main/java/httputils/MessageWrapper.java
@@ -5,18 +5,30 @@ import org.json.JSONObject;
 
 import java.util.Arrays;
 
+/**
+ * Class that contains methods to wrap message received into a JSON for the VNF chain
+ */
 public class MessageWrapper {
+
+    // private constructor because there is no need to create object of this class
     private MessageWrapper(){}
 
-    public static String wrapMessage(String originalMessage, String[] chain) {
-        JSONObject message = new JSONObject();
+    /**
+     * Method to puth a message inside a JSON to be exchange among links of the VNF chain
+     * @param message message that will be added to the JSON accessible with the key "message"
+     * @param chain list of the host that the message must pass thought accessible with the key "chain"
+     * @return String that represent the JSON created
+     */
+    // TODO check if other fields have to be added
+    public static String wrapMessage(String message, String[] chain) {
+        JSONObject jsonMessage = new JSONObject();
 
-        message.put("message", originalMessage);
+        jsonMessage.put("message", message);
         if (chain.length > 0) {
             JSONArray mJSONArray = new JSONArray(Arrays.asList(chain));
-            message.put("chain", mJSONArray);
+            jsonMessage.put("chain", mJSONArray);
         } else
-            message.put("chain", "[]");
-        return message.toString();
+            jsonMessage.put("chain", "[]");
+        return jsonMessage.toString();
     }
 }
