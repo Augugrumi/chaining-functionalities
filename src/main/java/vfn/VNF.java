2ae684ee821ea4a34ead744208c3725ba73c7017
mzanella <zanna0150@gmail.com>
2018-07-31 11:10:35 +0200

    Add comments to VNF

--- src/main/java/vfn/VNF.java
+++ src/main/java/vfn/VNF.java
@@ -1,5 +1,13 @@
 package vfn;
 
+/**
+ * Basic interface of a VNF object
+ */
 public interface VNF {
+    /**
+     * Function that is applied to the message
+     * @param message Message on which the function is applied
+     * @return String - Message transformed by the VNF
+     */
     String functionality(String message);
 }
